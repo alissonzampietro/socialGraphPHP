780bf89adea2fdbb6a3499abd9c12b5b470053a7
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-05-25 02:49:13 +0100

    feat: add suggested friends

--- socialGraph.php
+++ socialGraph.php
@@ -184,31 +184,3 @@ $socialData = array(
 		'cities' => array('Dublin' => 90, 'London' => 90)
 	)
 );
-
-$cities = $pdo->prepare("INSERT INTO cities(name) VALUES(?)");
-$getCity = $pdo->prepare("SELECT id FROM cities WHERE name = ?");
-$users_cities = $pdo->prepare("INSERT INTO users_cities(user_id, city_id, percentual) VALUES(?, ?, ?)");
-$pdo->beginTransaction();
-try {
-	foreach($socialData as $data) {
-		foreach($data['cities'] as $city => $percentual) {
-			$getCity->execute([$city]);
-			$result = $getCity->fetch();
-
-			if(!$result) {
-				$cities->execute([$city]);
-				$id = $pdo->lastInsertId();
-			}else {
-				$id = $result['id'];
-			}
-
-			$a = $users_cities->execute([$data['id'], $id, $percentual]);
-			var_dump('users_cities: ',$a);
-		}
-		
-	}
-	$pdo->commit();
-}catch(Exception $e) {
-	var_dump($e->getMessage());
-	exit;
-}
--- src/repositories/UserRepository.php
+++ src/repositories/UserRepository.php
@@ -16,14 +16,34 @@ class UserRepository {
     public function getAll() {
         $query = $this->instance->query('SELECT id, firstname, surname, age, gender FROM users');
         $query->execute();
-        return $this->attachTwoStepFriends($this->attachFriends($query->fetchAll(PDO::FETCH_ASSOC)));
+        return $this->attach($query->fetchAll(PDO::FETCH_ASSOC));
     }
 
     public function get($id) {
         $query = $this->instance->query('SELECT id, firstname, surname, age, gender FROM users WHERE id = ?');
         $query->execute([$id]);
-        $data = $this->attachFriends([$query->fetch(PDO::FETCH_ASSOC)]);
-        return $this->attachTwoStepFriends($data);
+        return $this->attach([$query->fetch(PDO::FETCH_ASSOC)]);
+    }
+
+    private function attach(array $users) {
+        foreach($users as &$user) {
+            $user['friends'] = $this->getFriends($user);
+            $friendsOfFriends = $this->getFriendsOfFriends($user);
+            $user['suggested_friends'] = $this->suggestedFriends($friendsOfFriends);
+            $user['friends_of_friend'] = $this->removeDuplicatedUsers($friendsOfFriends);
+        }
+
+        return $users;
+    }
+
+    private function getFriends(array $user) {
+        $select = $this->instance->query('SELECT userCon.* FROM users AS u 
+        INNER JOIN connections AS c ON u.id = c.user_id 
+        INNER JOIN users AS userCon ON c.user_connection_id = userCon.id 
+        WHERE u.id = ?');
+
+        $select->execute([$user['id']]);
+        return $select->fetchAll(PDO::FETCH_ASSOC);
     }
 
     private function getFriendsId($userId) {
@@ -37,38 +57,51 @@ class UserRepository {
         }, $query->fetchAll(PDO::FETCH_ASSOC));
     }
 
-    private function attachFriends(array $users) {
-        foreach($users as &$user) {
-            $select = $this->instance->query('SELECT userCon.* FROM users AS u 
-            INNER JOIN connections AS c ON u.id = c.user_id 
-            INNER JOIN users AS userCon ON c.user_connection_id = userCon.id 
-            WHERE u.id = ?');
+    private function getFriendsOfFriends(array $user) {
+        $friendsOfFriends = [];
+        $friendsId = $this->getFriendsId($user['id']);
+        foreach($user['friends'] as $friend) {
+            $select = $this->instance->query("SELECT userCon.* FROM users AS u 
+            INNER JOIN connections AS c ON u.id = c.user_id
+            INNER JOIN users AS userCon ON c.user_connection_id = userCon.id
+            WHERE u.id = ? AND c.user_connection_id NOT IN (?) AND c.user_connection_id != ?");
 
-            $select->execute([$user['id']]);
-            $user['friends'] = $select->fetchAll(PDO::FETCH_ASSOC);
+            $select->execute([$friend['id'], $friendsId, $user['id']]);
+            $friendsOfFriends = array_merge($friendsOfFriends, $select->fetchAll(PDO::FETCH_ASSOC));
+        }
+
+        return $friendsOfFriends;
+    }
+
+    public function removeDuplicatedUsers(array $users) {
+        $ids = [];
+
+        foreach($users as $key => $user) {
+            if(!in_array($user['id'], $ids)) {
+                array_push($ids, $user['id']);
+                continue;
+            }
+
+            unset($users[$key]);
         }
 
         return $users;
     }
 
-    private function attachTwoStepFriends(array $users) {
-        foreach($users as &$user) {
-            $friendsOfFriends = [];
-            $friendsId = $this->getFriendsId($user['id']);
-            foreach($user['friends'] as $friend) {
-                $select = $this->instance->query("SELECT userCon.* FROM users AS u 
-                INNER JOIN connections AS c ON u.id = c.user_id
-                INNER JOIN users AS userCon ON c.user_connection_id = userCon.id
-                WHERE u.id = ? AND c.user_connection_id NOT IN (?) AND c.user_connection_id != ?");
-
-                $select->execute([$friend['id'], $friendsId, $user['id']]);
-                $friendsOfFriends = array_merge($friendsOfFriends, $select->fetchAll(PDO::FETCH_ASSOC));
+    public function suggestedFriends(array $users) {
+        $duplicated = [];
+        $uniques = [];
+        foreach($users as $key => $user) {
+            if(!in_array($user['id'], $uniques)) {
+                array_push($uniques, $user['id']);
+                continue;
             }
 
-            $user['friends_of_friend'] = $friendsOfFriends;
+            array_push($duplicated, $user);
+            unset($users[$key]);
         }
 
-        return $users;
+        return $duplicated;
     }
 
 }
